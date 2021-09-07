99d8c8c2a024dbda6659479be8116a8dcbe1e462
binkesi <sjtusgn@163.com>
2021-09-07 16:04:17 +0800

    Start day2 edit, add dialect for compatible.

---
+++ day2-reflect-schema/cmd-test/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	weeorm "reflect-schema"
+	"reflect-schema/log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	engine, _ := weeorm.NewEngine("sqlite3", "wee.db")
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXIST User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	result, _ := s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Sungn", "Wumx").Exec()
+	count, _ := result.RowsAffected()
+	fmt.Printf("Exec success, %d affected\n", count)
+	row := s.Raw("SELECT Name FROM User LIMIT 1;").QueryRow()
+	var name string
+	row.Scan(&name)
+	log.Info(name)
+}
