a1e2450ed88eced8cb78dddba39a3ee3deb6a9e7
binkesi <sjtusgn@163.com>
2021-09-09 14:18:27 +0800

    Implement generator.

--- day3-save-query/clause/generator.go
+++ day3-save-query/clause/generator.go
@@ -35,12 +35,44 @@ func _insert(values ...interface{}) (string, []interface{}) {
 	return fmt.Sprintf("INSERT INTO %s, (%v)", tableName, fields), []interface{}{}
 }
 
-func _values(values ...interface{}) (string, []interface{}) {}
+func _values(values ...interface{}) (string, []interface{}) {
+	// VALUES ($v1), ($v2) ...
+	var bindStr string
+	var sql strings.Builder
+	var vars []interface{}
+	sql.WriteString("VALUES ")
+	for i, value := range values {
+		v := value.([]interface{})
+		if bindStr == "" {
+			bindStr = genBindVars(len(v))
+		}
+		sql.WriteString(fmt.Sprintf("%v", bindStr))
+		if i+1 != len(values) {
+			sql.WriteString(", ")
+		}
+		vars = append(vars, v...)
+	}
+	return sql.String(), vars
+}
 
-func _where(values ...interface{}) (string, []interface{}) {}
+func _where(values ...interface{}) (string, []interface{}) {
+	// WHERE $desc
+	desc, vars := values[0], values[1:]
+	return fmt.Sprintf("WHERE %s", desc), vars
+}
 
-func _orderby(values ...interface{}) (string, []interface{}) {}
+func _orderby(values ...interface{}) (string, []interface{}) {
+	return fmt.Sprintf("ORDER BY %s", values[0]), []interface{}{}
+}
 
-func _select(values ...interface{}) (string, []interface{}) {}
+func _select(values ...interface{}) (string, []interface{}) {
+	// SELECT $fields FROM $tableName
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ", ")
+	return fmt.Sprintf("SELECT %v FROM %s", fields, tableName), []interface{}{}
+}
 
-func _limit(values ...interface{}) (string, []interface{}) {}
+func _limit(values ...interface{}) (string, []interface{}) {
+	// LIMIT $num
+	return "LIMIT ?", values
+}
