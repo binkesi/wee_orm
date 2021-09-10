b360e79d6af97f3c4ab57611fc0b236213d5f7f2
binkesi <sjtusgn@163.com>
2021-09-10 11:11:40 +0800

    Add update, delete and count.

--- day4-chain-operation/clause/generator.go
+++ day4-chain-operation/clause/generator.go
@@ -17,6 +17,9 @@ func init() {
 	generators[ORDERBY] = _orderby
 	generators[SELECT] = _select
 	generators[LIMIT] = _limit
+	generators[UPDATE] = _update
+	generators[DELETE] = _delete
+	generators[COUNT] = _count
 }
 
 func genBindVars(nums int) string {
@@ -75,4 +78,24 @@ func _select(values ...interface{}) (string, []interface{}) {
 func _limit(values ...interface{}) (string, []interface{}) {
 	// LIMIT $num
 	return "LIMIT ?", values
+}
+
+func _update(values ...interface{}) (string, []interface{}) {
+	tableName := values[0]
+	m := values[1].(map[string]interface{})
+	var keys []string
+	var vars []interface{}
+	for k, v := range m {
+		keys = append(keys, k+" = ?")
+		vars = append(vars, v)
+	}
+	return fmt.Sprintf("UPDATE %s SET %s", tableName, strings.Join(keys, ", ")), vars
+}
+
+func _delete(values ...interface{}) (string, []interface{}) {
+	return fmt.Sprintf("DELETE FROM %s", values[0]), []interface{}{}
+}
+
+func _count(values ...interface{}) (string, []interface{}) {
+	return _select(values[0], []string{"count(*)"})
 }
