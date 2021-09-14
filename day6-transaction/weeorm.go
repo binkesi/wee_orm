3a365d1606dc08450ffa07a96a45ce25f83e45b2
binkesi <sjtusgn@163.com>
2021-09-14 17:35:04 +0800

    Add transaction support.

---
+++ day6-transaction/weeorm.go
@@ -0,0 +1,66 @@
+package weeorm
+
+import (
+	"database/sql"
+	"transaction/dialect"
+	"transaction/log"
+	"transaction/session"
+)
+
+type Engine struct {
+	db      *sql.DB
+	dialect dialect.Dialect
+}
+
+type TxFunc func(*session.Session) (interface{}, error)
+
+func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := engine.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			_ = s.Rollback() // err is non-nil; don't change it
+		} else {
+			err = s.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return f(s)
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s is not found", driver)
+		return
+	}
+	e = &Engine{db: db, dialect: dial}
+	log.Info("Connect database success")
+	return
+}
+
+func (e *Engine) Close() {
+	if err := e.db.Close(); err != nil {
+		log.Error(err)
+		return
+	}
+	log.Info("Database close success")
+}
+
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db, e.dialect)
+}
